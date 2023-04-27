eaf9e94bb7c82222c0b12c2421c00803aeceed46
El Obredor <obredorbre@gmail.com>
2023-04-27 08:53:41 -0500

    details

--- propiedades.blade.php
+++ propiedades.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     @vite('resources/css/app.css') @vite('resources/js/app.js')
-    <title>@yield('title')</title>
+    <title>propiedades</title>
 </head>
 
 <body>
